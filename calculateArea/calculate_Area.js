6343e435dbeaf040191e0806665500a492512021
LucasKelbes <lukekelbe@gmail.com>
2025-11-02 11:46:23 -0500

    extra

--- calculateArea/calculate_Area.js
+++ calculateArea/calculate_Area.js
@@ -5,7 +5,14 @@ function calculateArea() {
  width = parseFloat(document.getElementById('width').value);
 
  let area = length*width;
- document.getElementById('result').innerHTML = `The area of the rectangle is: $'
-area}`;
+ document.getElementById('result').innerText = `The area of the rectangle is: ${area}`;
 }
 
+function calculate_Groceries(){
+    grocieres1 = parseFloat(document.getElementById('Grocery-1').value);
+    grocieres2 = parseFloat(document.getElementById('Grocery-2').value);
+    grocieres3 = parseFloat(document.getElementById('Grocery-3').value);
+
+    let total = grocieres1+grocieres2+grocieres3;
+    document.getElementById('grocery_result').innerText = `The total amount is:$ ${total}`;
+}
